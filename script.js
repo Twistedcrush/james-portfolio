626dd10cf949dcce5a1c9f836e9ee2bbb6d6f7d4
Twistedcrush <117395267+Twistedcrush@users.noreply.github.com>
2026-02-25 11:31:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const text = "AI & Cybersecurity Specialist";
 let index = 0;
+// MATRIX EFFECT
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
 
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const letters = "01";
+const fontSize = 14;
+const columns = canvas.width / fontSize;
+
+const drops = [];
+
+for (let x = 0; x < columns; x++) {
+    drops[x] = 1;
+}
+
+function draw() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#00ff99";
+    ctx.font = fontSize + "px monospace";
+
+    for (let i = 0; i < drops.length; i++) {
+        const text = letters.charAt(Math.floor(Math.random() * letters.length));
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        drops[i]++;
+    }
+}
+
+setInterval(draw, 33);
 function typeEffect() {
     if (index < text.length) {
         document.getElementById("typing").innerHTML += text.charAt(index);
@@ -8,5 +43,6 @@ function typeEffect() {
         setTimeout(typeEffect, 80);
     }
 }
+
 
 window.onload = typeEffect;
