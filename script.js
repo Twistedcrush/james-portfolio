f1935a3aac2cb29ca31d064b6fd8f02ddc1b4fa9
Twistedcrush <117395267+Twistedcrush@users.noreply.github.com>
2026-02-25 10:46:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "AI & Cybersecurity Specialist";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 80);
+    }
+}
+
+window.onload = typeEffect;
